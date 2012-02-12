f7ff14a929672899ea65c8f0c40fcb2778b1a0af
Parabay <vishnuv@parabay.com>
2012-02-12 00:50:51 -0500

    remove files

--- express/app.js
+++ express/app.js
@@ -29,7 +29,8 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/start', routes.index);
+app.get('/start', routes.start);
+app.post('/upload', routes.upload);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- express/routes/index.js
+++ express/routes/index.js
@@ -1,8 +1,51 @@
 
+
+var querystring = require("querystring"),
+    fs = require("fs"),
+    formidable = require("formidable");
+
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+exports.start = function(request, response){
+  //res.render('index', { title: 'Express' })
+
+  console.log("Request handler 'start' was called.");
+
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+};
+
+
+exports.upload = function(request, response){
+	
+	console.log("Request handler 'upload' was called.");
+	
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+    fs.renameSync(files.upload.path, "/tmp/test.png");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
+    response.end();
+  });
+
 };
